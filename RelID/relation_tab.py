12cc7791392bbab73f6e6d37718e0ea43f8c96f4
romjeromealt <romejerome@orange.fr>
2025-09-25 18:05:52 +0200

    Fix a remaining issue if no default person

--- RelID/relation_tab.py
+++ RelID/relation_tab.py
@@ -322,7 +322,6 @@ class RelationTab(tool.Tool, ManagedWindow):
             box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
             window.add(box)
             ManagedWindow.__init__(self, uistate, [], self.__class__)
-            self.set_window(window, None, self.label)
 
             # Configuration du TreeView
             self.titles = [
@@ -358,13 +357,6 @@ class RelationTab(tool.Tool, ManagedWindow):
             quit_button.connect("clicked", self.quit_clicked)
             box.pack_end(quit_button, False, False, 0)
 
-        if uistate:
-            self.progress = ProgressMeter(self.label, can_cancel=False, parent=uistate.window)
-            #window.show_all()
-            self.set_window(window, None, self.label)
-        else:
-            self.progress = ProgressMeter(self.label)
-
         # Récupération de la personne par défaut
         default_person = self.dbstate.db.get_default_person()
         if default_person is None:
@@ -372,6 +364,14 @@ class RelationTab(tool.Tool, ManagedWindow):
             if uistate:
                 WarningDialog(_("No default person set."), parent=uistate.window)
             return
+
+        if uistate:
+            self.progress = ProgressMeter(self.label, can_cancel=False, parent=uistate.window)
+            #window.show_all()
+            self.set_window(window, None, self.label)
+        else:
+            self.progress = ProgressMeter(self.label)
+
 
         # Initialisation du filtre par défaut
         root_id = default_person.get_gramps_id()
